5fe63cde0d3cfefda166b4df3fe2539037a71426
Swaraj Rajesh Thanekar <swaraj.thanekar24@sanjivani.edu.in>
2024-08-26 00:07:32 +0530

    Add files via upload
    In this code we have 2 integer value and apply addition ,substration, multiplication, division and modulos operator

---
+++ Binary Operators.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+int a = 8, b = 3;
+
+// Addition operator
+
+cout << "a + b = " << (a + b) << endl;
+
+// Subtraction operator
+cout << "a - b = " << (a - b) << endl;
+
+// Multiplication operator
+cout << "a * b = " << (a * b) << endl;
+
+// Division operator
+cout << "a / b = " << (a / b) << endl;
+
+// Modulo operator
+cout << "a % b = " << (a % b) << endl;
+
+return 0;
+}
