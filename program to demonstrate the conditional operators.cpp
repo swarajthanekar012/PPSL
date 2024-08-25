b66d9f87400aae7d60bb020c5a6dfc9b5e2b772b
Swaraj Rajesh Thanekar <swaraj.thanekar24@sanjivani.edu.in>
2024-08-26 00:23:59 +0530

    Add files via upload
    The code used to select the greatest number from the given number in which the greater than operator is used in which the integer is takes as 3 and 4 which gives output as a number 4.

---
+++ program to demonstrate the conditional operators.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+int a=3,b=4;
+
+//Conditional operator
+int result=(a<b)?b:a;
+cout<<"The greatest number is"<<result<<endl;
+    return 0;
+}
